a0bc7133804a8a1750c12af431588003ead46195
themuku <murad.mesteliyev3@gmail.com>
2024-10-03 19:32:51 +0400

    made some changes in details page

--- src/pages/ProductDetailsPage.jsx
+++ src/pages/ProductDetailsPage.jsx
@@ -2,6 +2,16 @@ export default function ProductDetailsPage() {
   return (
     <>
       <h1>Product details page</h1>
+
+      <div>
+        <ul>
+          <li>element 1</li>
+          <li>element 2</li>
+          <li>element 3</li>
+          <li>element 4</li>
+          <li>element 5</li>
+        </ul>
+      </div>
     </>
   );
 }
