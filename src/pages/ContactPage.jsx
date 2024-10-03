2894e596298d420947bc58652767ea78a742e441
042Ali <ali20090609@gmail.com>
2024-10-03 19:20:25 +0400

    added form for contact  page

--- src/pages/ContactPage.jsx
+++ src/pages/ContactPage.jsx
@@ -2,6 +2,13 @@ export default function ContactPage() {
   return (
     <>
       <h1>Contact page</h1>
+      <form>
+        <label>name</label>
+        <input type="text" />
+        <br />
+        <label>email</label>
+        <input type="email" />
+      </form>
     </>
   );
 }
