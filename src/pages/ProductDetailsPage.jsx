a1c430d9d4e7c7420b6bb64eed212686ede1a666
user122436 <fidansadiqzade@gmail.com>
2024-10-03 19:34:11 +0400

    idk

--- src/pages/ProductDetailsPage.jsx
+++ src/pages/ProductDetailsPage.jsx
@@ -2,6 +2,7 @@ export default function ProductDetailsPage() {
   return (
     <>
       <h1>Product details page</h1>
+    <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Aperiam similique itaque ad aut incidunt, commodi optio quam quo facilis quas autem soluta unde ducimus ipsum quisquam atque dolores libero animi.</p>
     </>
   );
 }
