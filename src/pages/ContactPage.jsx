a79197c5b1fca7c81f19acca108cc424ce51496f
themuku <murad.mesteliyev3@gmail.com>
2024-10-11 19:26:35 +0400

    added map

--- src/pages/ContactPage.jsx
+++ src/pages/ContactPage.jsx
@@ -1,16 +1,18 @@
+import { Layout } from "antd";
+import ContactMap from "../components/contact/ContactMap";
+const { Content } = Layout;
+
 export default function ContactPage() {
   return (
     <>
-      <h1>Contact page</h1>
-      <form>
-        <label>name</label>
-        <input type="text" />
-        <br />
-        <label>email</label>
-        <input type="email" />
-        <label >message</label>
-        <textarea name="" id=""></textarea>
-      </form>
+      <Content
+        style={{
+          padding: "0 48px",
+        }}
+      >
+        <ContactMap />
+        {/* <ContactForm /> */}
+      </Content>
     </>
   );
 }
